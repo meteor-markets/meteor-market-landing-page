b6440dac49f4311880559964d5fe8c54172ed125
Mithun Rajput <mithunchauhan03721@gmail.com>
2024-02-07 20:47:26 +0530

    code merge

--- src/views/pages/HomePage/WhyMeteorMarkets.js
+++ src/views/pages/HomePage/WhyMeteorMarkets.js
@@ -11,9 +11,11 @@ import {
 const useStyles = makeStyles((theme) => ({
   headBox: {
     backgroundColor:"#343434",
-    padding:"20px",
+    padding:"25px",
+    borderRadius:"19px",
     "& h1":{
-        textAlign:"center"
+        textAlign:"center",
+        marginBottom:"3rem"
     },
     "& h3":{
         marginBottom:"1.5rem"
@@ -28,10 +30,27 @@ function WhyMeteorMarkets() {
     <Container>
       <Box className={classes.headBox}>
       <Typography variant="h1">Why Meteor Markets?</Typography>
-        <Grid container>
+        <Grid container spacing={4}>
           <Grid item xs={12} sm={6} md={4}>
             <Box>
                 <Typography variant="h3">Zero-fee Borrowing</Typography>
+                <Typography variant="body1" style={{color:"#fff"}}>
+                By leveraging Blast's native yield <br/> functionality, Meteor Markets enables <br/> borrowing at zero fees.<br/>
+                Talk about risk-free leverage.
+                </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Box>
+                <Typography variant="h3">Lending</Typography>
+                <Typography variant="body1" style={{color:"#fff"}}>
+                Multi-asset lending on Meteor<br/> Markets allows users to earn <br/>yield. Say no to idle capital.
+                </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Box>
+                <Typography variant="h3">Automate</Typography>
                 <Typography variant="body1" style={{color:"#fff"}}>
                 By leveraging Blast's native yield <br/> functionality, Meteor Markets enables <br/> borrowing at zero fees.<br/>
                 Talk about risk-free leverage.
