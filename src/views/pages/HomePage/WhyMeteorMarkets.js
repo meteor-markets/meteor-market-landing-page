52b7b0ffa479115b300a843aa656d6bfc8c65123
Mithun Rajput <mithunchauhan03721@gmail.com>
2024-02-07 09:45:41 +0530

    Added heading in why meteor markets

--- src/views/pages/HomePage/WhyMeteorMarkets.js
+++ src/views/pages/HomePage/WhyMeteorMarkets.js
@@ -10,7 +10,14 @@ import {
 
 const useStyles = makeStyles((theme) => ({
   headBox: {
-    // padding:"8rem 0rem"
+    backgroundColor:"#343434",
+    padding:"20px",
+    "& h1":{
+        textAlign:"center"
+    },
+    "& h3":{
+        marginBottom:"1.5rem"
+    }
   },
 }));
 
@@ -20,8 +27,17 @@ function WhyMeteorMarkets() {
   return (
     <Container>
       <Box className={classes.headBox}>
+      <Typography variant="h1">Why Meteor Markets?</Typography>
         <Grid container>
-          <Grid item xs={12} sm={6} md={4}></Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Box>
+                <Typography variant="h3">Zero-fee Borrowing</Typography>
+                <Typography variant="body1" style={{color:"#fff"}}>
+                By leveraging Blast's native yield <br/> functionality, Meteor Markets enables <br/> borrowing at zero fees.<br/>
+                Talk about risk-free leverage.
+                </Typography>
+            </Box>
+          </Grid>
         </Grid>
       </Box>
     </Container>
